0484a66214aa25dca151a73dd9629c143c9aa927
tilammm <tilam403@gmail.com>
2019-12-03 00:03:53 +0300

    file size

--- storage_node/storage_node.py
+++ storage_node/storage_node.py
@@ -144,7 +144,7 @@ class ClientReader(Thread):
         filename = self.sock.recv(1024).decode()
         f = open(filename, 'rb')
         print(f)
-        file_size = f.tell()
+        file_size = os.path.getsize(filename)
         print(file_size)
         self.sock.send(str(file_size).encode())
 
